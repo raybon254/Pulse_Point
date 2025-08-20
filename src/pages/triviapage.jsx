65b77d86a4d3167b41bf3385e3d35c08af2c11eb
Brian <musyimib449@gmail.com>
2025-08-21 02:44:57 +0300

    component bundling to structulize the trivia layout page(card,category)

---
+++ src/pages/triviapage.jsx
@@ -0,0 +1,13 @@
+import Cardinterface from "../components/trivia/card";
+import Categories from "../components/trivia/category";
+
+function Trivia() {
+    return (
+        <>
+        <Cardinterface />
+        <Categories />
+        </>
+    )
+}
+
+export default Trivia;
