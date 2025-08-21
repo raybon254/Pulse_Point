afb456239b83bf5511d84aa84e42fe014514d83f
Brian <musyimib449@gmail.com>
2025-08-21 03:17:31 +0300

    Side display styling

--- src/components/trivia/category.jsx
+++ src/components/trivia/category.jsx
@@ -1,37 +1,47 @@
-import { Container, Row, Col, Image } from "react-bootstrap";
-import { creativity, entertainment, fun, general, sports } from "../assets"; 
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
+import Cardinterface from "./card";
+import creativity from "../../assets/creativity.jpg";
+import entertainment2 from "../../assets/entertainment2.jpeg";
+import fun from "../../assets/fun.jpg";
+import general from "../../assets/general.jpeg";
+import sports from "../../assets/sports.jpg";
 
 function Categories() {
   return (
     <Container fluid className="my-4">
       <Row>
-        {/* Left Column */}
-        <Col xs={12} md={6}>
-          <Container className="d-flex align-items-center mb-3">
-            <Image src={general} alt="Upload Error" fluid rounded style={{ width: "50px", height: "auto" }} />
+        {/* Left Sidebar */}
+        <Col xs={12} md={2} className="d-flex flex-column align-items-start p-2" style={{background : "#344747ff"}}>
+          <Container className="d-flex flex-column align-items-center mb-3">
+            <Image src={general} alt="Upload Error" fluid rounded style={{ width: "100px", height: "auto" }} className="mb-2"/>
             <span className="ms-2 fw-bold">General Knowledge</span>
           </Container>
 
-          <Container className="d-flex align-items-center mb-3">
-            <Image src={entertainment} alt="Upload Error" fluid rounded style={{ width: "50px", height: "auto" }} />
+          <Container className="d-flex flex-column align-items-center mb-3">
+            <Image src={entertainment2} alt="Upload Error" fluid rounded style={{ width: "100px", height: "auto" }} className="mb-2"/>
             <span className="ms-2 fw-bold">Entertainment & Media</span>
           </Container>
 
-          <Container className="d-flex align-items-center mb-3">
-            <Image src={sports} alt="Upload Error" fluid rounded style={{ width: "50px", height: "auto" }} />
+          <Container className="d-flex flex-column align-items-center mb-3">
+            <Image src={sports} alt="Upload Error" fluid rounded style={{ width: "100px", height: "auto" }} className="mb-2"/>
             <span className="ms-2 fw-bold">Sports & Lifestyle</span>
           </Container>
         </Col>
 
-        {/* Right Column */}
-        <Col xs={12} md={6}>
-          <Container className="d-flex align-items-center mb-3">
-            <Image src={creativity} alt="Upload Error" fluid rounded style={{ width: "50px", height: "auto" }} />
+        {/* Center Content */}
+        <Col xs={12} md={8} className="d-flex justify-content-center align-items-center bg-white">
+          <Cardinterface />
+        </Col>
+
+        {/* Right Sidebar */}
+        <Col xs={12} md={2} className="d-flex flex-column align-items-start p-3" style={{background : "#344747ff"}}>
+          <Container className="d-flex flex-column align-items-center mb-3">
+            <Image src={creativity} alt="Upload Error" fluid rounded style={{ width: "100px", height: "auto" }} className="mb-2"/>
             <span className="ms-2 fw-bold">Mind & Creativity</span>
           </Container>
 
-          <Container className="d-flex align-items-center mb-3">
-            <Image src={fun} alt="Upload Error" fluid rounded style={{ width: "50px", height: "auto" }} />
+          <Container className="d-flex flex-column align-items-center mb-3">
+            <Image src={fun} alt="Upload Error" fluid rounded style={{ width: "100px", height: "auto" }} className="mb-2"/>
             <span className="ms-2 fw-bold">Fun & Theme</span>
           </Container>
         </Col>
