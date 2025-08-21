6096200916620e39f2e116f8248683e951320251
Brian <musyimib449@gmail.com>
2025-08-21 03:18:25 +0300

    dropped card import intergrated to category

--- src/pages/triviapage.jsx
+++ src/pages/triviapage.jsx
@@ -1,10 +1,8 @@
-import Cardinterface from "../components/trivia/card";
 import Categories from "../components/trivia/category";
 
 function Trivia() {
     return (
         <>
-        <Cardinterface />
         <Categories />
         </>
     )
