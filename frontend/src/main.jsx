1fe87d573eda5227d0251df550b097c770f293a9
Brian <musyimib449@gmail.com>
2025-09-09 18:49:33 +0300

    Build🛠️: main.jsx browser router implementation to facilitate link navigation

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QuizProvider } from "./contexts/QuizContext.jsx"; 
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QuizProvider>
+        <App />
+      </QuizProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
